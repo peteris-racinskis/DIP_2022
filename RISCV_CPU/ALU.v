1372c2032e0fe230958ff7d629f9e63d94c2f592
peteris racinskis <peterisracinskis@gmail.com>
2022-03-01 17:56:05 +0200

    ALU tests working, flags working, should be fine now.

--- RISCV_CPU/ALU.v
+++ RISCV_CPU/ALU.v
@@ -18,37 +18,59 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module ALU(A,B,OP,R);
+module ALU(A,B,OP,OUT,V,Z,N,C);
 	
 	parameter WORDSIZE = 32;
-	parameter OPSIZE = 32;
-	parameter UI = 32 - 12;
+	parameter OPSIZE = $clog2(11);
+	parameter IMMSIZE = 20;
+	parameter UI = WORDSIZE - IMMSIZE;
 	
 	input [WORDSIZE-1:0] A,B;
 	input [OPSIZE-1:0]	OP;
-	output reg [WORDSIZE-1:0]	R;
-	// if this doesn't work use (A < B) & !(sig(A) ^ sig(B))
+	output reg signed [WORDSIZE-1:0]	OUT;
+	output V,N,Z;
+	output reg C;
 	wire signed [WORDSIZE-1:0] AA, BB;
+	wire signA, signB, signOUT;
 	
 	assign AA = A;
 	assign BB = B;
+	assign signA = A[WORDSIZE-1];
+	assign signB = B[WORDSIZE-1];
+	assign signOUT = OUT[WORDSIZE-1];
+	
+	parameter ADD = 1;
+	parameter SUB = 2;
+	parameter SLL = 3;
+	parameter SRL = 4;
+	parameter SRA = 5;
+	parameter SLU = 6;	// set less than unsigned
+	parameter SLT = 7;	// set less than
+	parameter OR = 8;
+	parameter AND = 9;
+	parameter XOR = 10;
+	parameter SIU = 11; // Shift immediate to upper
 	
 	always @(*)
 	begin
 		case (OP)
-			0	: R = A + B;			// ADD
-			1	: R = A - B;			// SUB
-			3	: R = A << B;			// SLA/SLL
-			4	: R = A >> B;			// SRL
-			5	: R = A >>> B;			// SRA
-			6	: R = A < B;			// SLTU 
-			7	: R = AA < BB ;		// SLT 
-			8	: R = A | B;			// OR
-			9	: R = A & B;			// AND
-			10	: R = A ^ B;			// XOR
-			11	: R = ((A << UI) | 0);	// Shift immediate to upper
-			default: R = 0;
+			ADD: {C,OUT} = A + B;
+			SUB: {C,OUT} = A - B;
+			SLL: OUT = A << B;
+			SRL: OUT = A >> B;
+			SRA: OUT = A >>> B;
+			SLU: OUT = A < B;
+			SLT: OUT = AA < BB;
+			OR : OUT = A | B;
+			AND: OUT = A & B;
+			XOR: OUT = A ^ B;
+			SIU: OUT = A << UI;
+			default: OUT = 0;
 		endcase
 	end
-
+	
+	assign V = (OP == ADD) & (signA == signB) & (signOUT != signA);
+	assign N = OUT[WORDSIZE-1];
+	assign Z = !(|OUT);
+	
 endmodule
--- RISCV_CPU/ALU_test.v
+++ RISCV_CPU/ALU_test.v
@@ -23,24 +23,149 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 module ALU_test;
-
+	
+	parameter ADD = 1;
+	parameter SUB = 2;
+	parameter SLL = 3;
+	parameter SRL = 4;
+	parameter SRA = 5;
+	parameter SLU = 6;	// set less than unsigned
+	parameter SLT = 7;	// set less than
+	parameter OR = 8;
+	parameter AND = 9;
+	parameter XOR = 10;
+	parameter SIU = 11; // Shift immediate to upper
+	
+	parameter WORDSIZE = 32;
+	parameter IMMSIZE = 20;
+	parameter OPSIZE = $clog2(11);
+	parameter UI = WORDSIZE-IMMSIZE;
+	parameter maxint_u = {WORDSIZE{1'b1}};
+	parameter maxint_s = {WORDSIZE-1{1'b1}};
+	
 	// Inputs
-	integer A;
-	integer B;
-	reg [31:0] OP;
+	reg [WORDSIZE-1:0] A, B;
+	integer aa;
+	integer bb;
+	integer i;
+	reg [OPSIZE-1:0] OP;
 
 	// Outputs
-	wire [31:0] R;
+	wire [WORDSIZE-1:0] R;
+	wire C,V,Z,N;
 
 	// Instantiate the Unit Under Test (UUT)
 	ALU uut (
 		.A(A), 
 		.B(B), 
 		.OP(OP), 
-		.R(R)
+		.OUT(R),
+		.V(V),
+		.Z(Z),
+		.N(N),
+		.C(C)
 	);
 
 	// WRITE SOME SERIOUS TEST ROUTINES TOMORROW.
+	task test_logic(input integer AA, BB);
+		begin
+			A = AA;
+			B = BB;
+			OP = ADD;
+			#5;
+			if (R != AA + BB) begin
+				$display("Error: %0d + %0d = %0d",AA,BB,R);
+			end
+			#20 OP = SUB;
+			#5;
+			if (R != AA - BB) begin
+				$display("Error: %0d - %0d = %0d",AA,BB,R);
+			end
+			#20 OP = SLL;
+			#5;
+			if (R != AA << BB) begin
+				$display("Error: %0b << %0d = %0b",AA,BB,R);
+			end
+			#20 OP = SRL;
+			#5;
+			if (R != AA >> BB) begin
+				$display("Error: %0b >> %0d = %0b",AA,BB,R);
+			end
+			#20 OP = SRA;
+			#5;
+			if (R != AA >>> BB) begin
+				$display("Error: %0b >>> %0d = %0b",AA,BB,R);
+			end
+			#20 OP = SLU;
+			#5;
+			if (R != (A < B)) begin
+				$display("Error: %0d < %0d = %0d (unsigned)",AA,BB,R);
+			end
+			#20 OP = SLT;
+			#5;
+			if (R != (AA < BB)) begin
+				$display("Error: %0d < %0d = %0d",AA,BB,R);
+			end
+			#20 OP = OR;
+			#5;
+			if (R != (A | B)) begin
+				$display("Error: %0b | %0b = %0b; expected = %0b",AA,BB,R,AA|BB);
+			end
+			#20 OP = AND;
+			#5;
+			if (R != (A & B)) begin
+				$display("Error: %0b & %0b = %0b; expected = %0b",AA,BB,R,AA&BB);
+			end
+			#20 OP = XOR;
+			#5;
+			if (R != (A ^ B)) begin
+				$display("Error: %0b ^ %0b = %0b",AA,BB,R);
+			end
+			#20 OP = SIU;
+			#5;
+			if (R != AA << UI) begin
+				$display("Error: %0b << %0d = %0b",AA,UI,R);
+			end
+		end
+	endtask
+	
+	task test_flags(input integer AA, BB);
+		integer result;
+		begin
+			A = AA;
+			B = BB;
+			#10 OP = ADD;
+			result = AA + BB;
+			#10;
+			if ((result == 0) & !Z) begin
+				$display("Error: %0d + %0d = %0d; Zero flag not triggered",AA,BB,R);
+			end
+			if ((result < 0) & !N) begin
+				$display("Error: %0d + %0d = %0d; Negative flag not triggered",AA,BB,R);
+			end
+			if ((result > maxint_u) & !C) begin
+				$display("Error: %0b + %0b = %0b; Carry flag not triggered",AA,BB,R);
+			end
+			if ((result > maxint_s) & !V) begin
+				$display("Info: %0b + %0b = %0b; Overflow flag not triggered",AA,BB,R);
+			end
+			#10 OP = SUB;
+			result = AA - BB;
+			#10;
+			if ((result == 0) & !Z) begin
+				$display("Error: %0d + %0d = %0d; Zero flag not triggered",AA,BB,R);
+			end
+			if ((result < 0) & !N) begin
+				$display("Error: %0d + %0d = %0d; Negative flag not triggered",AA,BB,R);
+			end
+			if ((result > maxint_u) & !C) begin
+				$display("Error: %0b + %0b = %0b; Carry flag not triggered",AA,BB,R);
+			end
+			if ((result > maxint_s) & !V) begin
+				$display("Info: %0b + %0b = %0b; Overflow flag not triggered",AA,BB,R);
+			end
+		end
+	endtask
 	
 	initial begin
 		// Initialize Inputs
@@ -50,26 +175,25 @@ module ALU_test;
 
 		// Wait 100 ns for global reset to finish
 		#100;
-      A = -2;
-		B = 2;
-		#100;
-		OP = 1;
-		#100;
-		OP = 2;
-		#100;
-		OP = 3;
-		#100;
-		OP = 4;
-		#100;
-		OP = 5;
-		#100;
-		OP = 6;
-		#100;
-		OP = 7;
-		#100;
-		OP = 8;
+		aa = maxint_u;
+		bb = maxint_u;
+		#10;
+		test_flags(aa,bb);
+		#50;
+		aa = maxint_s;
+		bb = maxint_s;
+		#10;
+		test_flags(aa,bb);
+		#50;
+		test_logic(0,0);
+		test_flags(0,0);
+		test_flags(0,1); // check carry flag for subtract
 		// Add stimulus here
-
+		for (i=1;i<32;i=i+1) begin
+			aa = 2 * aa;
+			bb = -1+i;
+			test_logic(aa,bb);
+		end
 	end
       
 endmodule
